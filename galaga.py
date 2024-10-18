a6ff547c81721e33dcab6b4fecdc66913dc41ea1
tipchuwis <a7157@palmore.edu.mx>
2024-10-18 09:25:50 -0600

    Jugador establecido

--- galaga.py
+++ galaga.py
@@ -1,16 +1,29 @@
 import pygame 
+from ship import Ship # Del archivo de ship, importa la clase Ship, que es nuestro jugador.
+
 
 display = pygame.display.set_mode((640, 480)) # Utilizamos métodos y clases de pygame para hacer la pantalla. La sintáxis es altura por anchura.
 running = True # Booleano que nos permite CORRER el juego.
 fps = 60 # Variable para limitar los fps.
 clock = pygame.time.Clock() # Utilizamos pygame para limitar los ticks en el juego.
+player = Ship() # Nuestro jugador es la nave, por lo que habrá una variable que acceda a la clase importada.
 
+sprite_group = pygame.sprite.Group() # Accedemos a la propiedad sprite de pygame para poder mover a un grupo de
+sprite_group.add(player)             # sprites, y le agregamos el método .Group(). Añadimos al jugador al grupo.
 
 while running: # Mientras que running es cierto, es decir, mientras SIGA CORRIENDO EL JUEGO, actualiza la pantalla.
+    # Tick Clock
     clock.tick(fps) # Especificamos los fps con nuestras respectivas variables.
+    # Handle Events
     for event in pygame.event.get(): # Por cada evento que pase en el display, guárdalo,
        if event.type == pygame.QUIT:# y si te dicen que lo cierres, pues cierra el jueguito.
             pygame.quit() 
             quit()
+    
+    # Actualizamos los objetos / Update all the objects
+    sprite_group.update()
+
+    # Renderizamos la pantalla / Render the display
+    sprite_group.draw(display)
 
     pygame.display.update()
---
+++ ship.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Ship(pygame.sprite.Sprite): # Esta es la clase que va a hacer que nuestra navecita se genere y aparezca
+    def __init__(self):           # en la pantalla, también definimos la velocidad inicial de la nave en x y y.
+        super(Ship, self).__init__() # Y comenzamos a importar la imagen que usaremos para la nave.
+        self.image = pygame.image.load('ship.png').convert_alpha() # conver_alpha() sirve para que se optimice la
+        self.rect = self.image.get_rect()                          # imagen en el juego y continue siendo transparente.
+        self.vel_x = 0        # .get_rect() consigue las propiedades rectangulares de la imagen para que podamos
+        self.vel_y = 0        # acceder a ellas fácilmente más tarde.
+        self.speed = 5
+
+    def update(self): # Esta clase va a correr los frames de nuestro objeto, que es la nave.
+        self.rect.x += self.vel_x # Por cada frame del juego, la coordenada en x y/o y se actualiza y aparenta
+        self.rect.y += self.vel_y # estar animado el objeto.
